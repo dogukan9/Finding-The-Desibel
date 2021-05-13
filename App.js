213cf97362a5a0fabd08a6d9075f2183f1d9da67
dogukan9 <dogukan1999.dune@gmail.com>
2021-05-13 17:54:45 +0300

    store updated

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './Navigator/navigator';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import reducer from './Store/reducer/reducer';
+const rootReducer = combineReducers({
+  reducer: reducer,
+});
 
+const store = createStore(rootReducer);
 export default function App() {
   return (
-    
+    <Provider store={store}>
       <NavigationContainer>
         <StackNavigation />
       </NavigationContainer>
-    
+    </Provider>
   );
 }
